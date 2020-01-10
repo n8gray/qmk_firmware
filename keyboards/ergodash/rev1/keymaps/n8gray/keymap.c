5489a131d5b53abbbb1eddcf921adb286f85fff2
Nathan Gray <n8gray@n8gray.org>
2020-01-10 10:48:18 -0800

    Add a SYMBOLS layer
    It basically maps the symbols from the number keys down to the home row.
    Also maps the numbers to one row above home and the F keys to the number row.
    I still have some issues to figure out about which modifiers to use to access the layers
    but this is getting closer to finished.

--- keyboards/ergodash/rev1/keymaps/n8gray/keymap.c
+++ keyboards/ergodash/rev1/keymaps/n8gray/keymap.c
@@ -4,15 +4,48 @@
 enum custom_keycodes {
 	PREV_TAB = SAFE_RANGE,
 	NEXT_TAB,
+    LOWER,
+    RAISE,
+    ADJUST,
+};
+
+enum layer_names {
+  _QWERTY = 0,
+  _NAV,
+  _SYMBOLS,
+  _ADJUST,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_4key(KC_ESCAPE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_MINS, KC_EQL, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_LPRN, KC_RPRN, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_GRAVE, KC_RGUI, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, KC_RSPC, KC_CAPS, KC_MUTE, KC_LGUI, KC_LALT, KC_RGUI, TT(1), KC_DEL, KC_ENT, TT(2), KC_SPC, KC_SLASH, KC_LEFT, KC_DOWN, KC_RGHT),
-	[1] = LAYOUT_4key(_______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, _______, _______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F12, _______, _______, KC_HOME, KC_UP, KC_END, KC_PGUP, _______, _______, KC_PGUP, KC_HOME, KC_UP, KC_END, _______, KC_F11, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_LBRACKET, KC_RBRACKET, _______, _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_PGUP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UNDS, _______, KC_HOME, KC_PGDOWN, KC_END),
-	[2] = LAYOUT_4key(_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, _______, RGB_MOD, RGB_RMOD, _______, KC_TRNS, _______, _______, KC_TRNS, _______, _______, _______, _______, _______)
+	[_QWERTY] = LAYOUT_4key(
+	   KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_MINS,                 KC_EQL,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
+	   KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_LBRC,                KC_RBRC,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
+	  KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_LPRN,                KC_RPRN,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, 
+	  KC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_GRV,                KC_RGUI,    KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_UP, KC_RSPC,
+	  KC_CAPS, KC_MUTE,   TT(3), KC_LALT,          KC_LGUI,   TT(1), KC_DEL, KC_ENT,  TT(2),  KC_SPC,         KC_SLASH, KC_LEFT, KC_DOWN, KC_RGHT
+	  ),
+	[_NAV] = LAYOUT_4key(
+		_______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, _______, 						_______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F12, 
+		_______, _______, KC_HOME, KC_UP, KC_END, KC_PGUP, _______, 				_______, KC_PGUP, KC_HOME, KC_UP, KC_END, _______, KC_F11, 
+		_______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, 				_______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_LBRACKET, KC_RBRACKET, 
+		_______, _______, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, _______, 				_______, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_PGUP, _______, 
+		_______, _______, _______, _______, _______, _______, _______, 			    _______, _______, KC_UNDS, _______, KC_HOME, KC_PGDOWN, KC_END
+		),
+	[_SYMBOLS] = LAYOUT_4key(
+		_______, _______, _______, _______, _______, _______, _______, 				_______, _______, _______, _______, _______, _______, _______, 
+		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, _______, 							_______, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, 
+		KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, _______, 				_______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
+		_______, _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, _______, 				_______, KC_LCBR, KC_RCBR, KC_LBRACKET, KC_RBRACKET, KC_PGUP, _______, 
+		_______, _______, _______, _______, _______, _______, _______, 				_______, _______, KC_MINUS, _______, KC_HOME, KC_PGDOWN, KC_END
+		),
+	[_ADJUST] = LAYOUT_4key(
+		_______, _______, _______, _______, _______, _______, _______, 				_______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, 				_______, _______, _______, _______, _______, _______, _______,
+		RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______, _______, _______, 				_______, _______, _______, _______, _______, _______, _______,
+		RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, _______, _______, _______, 				_______, _______, _______, _______, _______, _______, _______,
+		RGB_TOG, RGB_MOD, _______, RGB_RMOD, _______, _______, _______, 			_______, _______, _______, _______, _______, _______, _______
+		)
 };
-
-#define BASE_LAYER 0
 
 //#if 0
 
@@ -31,12 +64,18 @@ static rgblight_segment_t layer2_segments[] = {
 	{12, 2, 85, 255, LAYER_VAL}, 
 	RGBLIGHT_END_SEGMENTS
 };
+static rgblight_segment_t layer3_segments[] = {
+	{10, 1, 40, 255, LAYER_VAL}, 
+	{13, 1, 40, 255, LAYER_VAL}, 
+	RGBLIGHT_END_SEGMENTS
+};
 
 // Now define the array of layers. Later layers take precedence
 static rgblight_segment_t * const mod_led_layers[] = {
 	capslock_segments,
 	layer1_segments,
 	layer2_segments,
+	layer3_segments,
 	NULL
 };
 
@@ -49,8 +88,10 @@ void keyboard_post_init_user(void) {
 
 // Use the RGBLight layers feature to indicate active layers
 uint32_t layer_state_set_user(uint32_t state) {
+	//uprintf("layer state: %d\n", state);
 	rgblight_set_layer_state(1, BIT_SET(state, 1));
 	rgblight_set_layer_state(2, BIT_SET(state, 2));
+	rgblight_set_layer_state(3, BIT_SET(state, 3));
 	// This isn't required but it improves the responsiveness
 	rgblight_set();
   	return state;
