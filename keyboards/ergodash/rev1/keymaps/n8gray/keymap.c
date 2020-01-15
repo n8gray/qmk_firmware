4ecf7d85ebc44d384d3c8b8c67e6ee53d7b0dde4
Nathan Gray <n8gray@n8gray.org>
2020-01-15 14:35:35 -0800

    No more space cadet shift. Never got used to it.

--- keyboards/ergodash/rev1/keymaps/n8gray/keymap.c
+++ keyboards/ergodash/rev1/keymaps/n8gray/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	   KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_MINS,                 KC_EQL,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
 	   KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_LBRC,                KC_RBRC,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
 	  KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_LPRN,                KC_RPRN,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, 
-	  KC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_GRV,                KC_RGUI,    KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_UP, KC_RSPC,
+	  KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_GRV,                KC_RGUI,    KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_UP, KC_RSFT,
 	  KC_CAPS, KC_MUTE,   TT(3), KC_LALT,          KC_LGUI,   TT(1), KC_DEL, KC_ENT,  TT(2),  KC_SPC,         KC_SLASH, KC_LEFT, KC_DOWN, KC_RGHT
 	  ),
 	[_NAV] = LAYOUT_4key(
@@ -33,8 +33,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		),
 	[_SYMBOLS] = LAYOUT_4key(
 		_______, _______, _______, _______, _______, _______, _______, 				_______, _______, _______, _______, _______, _______, _______, 
-		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, _______, 							_______, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, 
-		KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, _______, 				_______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
+		 KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, _______, 							_______, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, 
+		KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, _______, 				_______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
 		_______, _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, _______, 				_______, KC_LCBR, KC_RCBR, KC_LBRACKET, KC_RBRACKET, KC_PGUP, _______, 
 		_______, _______, _______, _______, _______, _______, _______, 				_______, _______, KC_MINUS, _______, KC_HOME, KC_PGDOWN, KC_END
 		),
