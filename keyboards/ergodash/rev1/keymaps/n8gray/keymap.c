39ada5cefa5d04a75a927e27c462344139058d0e
Nathan Gray <n8gray@n8gray.org>
2020-01-27 10:56:28 -0800

    Replace del on left thumb with space
    Various UIs assume your left thumb has access to the space bar and combine it with the mouse.
    This is easier than rebinding everything, and I never used that del anyway.

--- keyboards/ergodash/rev1/keymaps/n8gray/keymap.c
+++ keyboards/ergodash/rev1/keymaps/n8gray/keymap.c
@@ -21,7 +21,7 @@ enum layer_names {
 #define SYMBOL_LAYER2		 KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_MPRV, 				KC_VOLD,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS
 #define SYMBOL_LAYER3		KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_MPLY, 				KC_MUTE, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS
 #define SYMBOL_LAYER4		_______, KC_PSLS, KC_PMNS, KC_PEQL, KC_PPLS, KC_PAST, _______, 				_______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_PGUP, _______
-#define SYMBOL_LAYER5		_______, _______, _______, _______, _______, _______, _______, 				_______, _______, KC_UNDS, _______, KC_HOME, KC_PGDOWN, KC_END
+#define SYMBOL_LAYER5		_______, _______, _______, _______, _______, _______, KC_PMNS, 				_______, _______, KC_UNDS, _______, KC_HOME, KC_PGDOWN, KC_END
 
 #define EXPAND_LAYER(l1, l2, l3, l4, l5) LAYOUT_4key(l1, l2, l3, l4, l5)
 
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	   KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_LBRC,                KC_RBRC,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
 	  KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_DEL,                KC_BSPC,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, 
 	  KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_GRV,                KC_RGUI,    KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_UP, KC_RSFT,
-	  KC_CAPS, KC_MUTE,   TT(_NAV), KC_LALT,          KC_LGUI,   LOWER, KC_DEL, KC_ENT,  RAISE,  KC_SPC,         KC_SLASH, KC_LEFT, KC_DOWN, KC_RGHT
+	  KC_CAPS, KC_MUTE,   TT(_NAV), KC_LALT,       KC_LGUI,   LOWER, KC_SPC, KC_ENT,  RAISE,  KC_SPC,         KC_SLASH, KC_LEFT, KC_DOWN, KC_RGHT
 	  ),
 	[_LOWER] = EXPAND_LAYER(
 		SYMBOL_LAYER1,
